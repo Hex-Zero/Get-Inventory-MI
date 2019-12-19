2a5d55b68a95b267828ececc1d094ca8338588f1
Hex-Zero <hex0@live.com>
2019-12-19 12:42:02 +0000

    Add MongoDB.driver , inv class.

---
+++ Get Inventory MI/Models/Inv.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Get_Inventory_MI.Models
+{
+    public class Inv
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Sku")]
+        public string Sku { get; set; }
+
+        [BsonElement("Inventory")]
+        public string Inventory { get; set; }
+
+    }
+}
